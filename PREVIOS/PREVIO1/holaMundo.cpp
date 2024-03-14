5927ab90ef88c22b682e68fd032be5979a8873de
eve2030f <143893444+eve2030f@users.noreply.github.com>
2024-03-14 14:35:01 -0600

    Actualizando el holaMundo

---
+++ PREVIOS/PREVIO1/holaMundo.cpp
@@ -0,0 +1,9 @@
+// Your First C++ Program B82870
+
+# include <iostream>
+
+int main( ) {
+	    std:: cout<< "Hola Mundo, estudiantes de IE0217! \n";
+		std:: cout<< "Probando el stash!";
+    return 0;
+	}
