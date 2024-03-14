d7ff72529b71c3d53e55b11f698b8c2a68f2a3bb
eve2030f <143893444+eve2030f@users.noreply.github.com>
2024-03-14 14:34:21 -0600

    Delete PREVIOS/PREVIO1/holaMundo.cpp

--- PREVIOS/PREVIO1/holaMundo.cpp
+++
@@ -1,9 +0,0 @@
-// Your First C++ Program B82870
-
-# include <iostream>
-
-int main( ) {
-	    std:: cout<< "Hola Mundo, estudiantes de IE0217! \n";
-		std:: cout<< "Probando el stash!";
-    return 0;
-	}
