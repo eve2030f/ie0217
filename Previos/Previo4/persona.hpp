a4ab5d8cf04f8ed644eb28c15cb58ffbd3367989
Evelyn Feng <evelyn2000feng@gmail.com>
2024-04-01 09:11:34 -0600

    Persona
    Signed-off-by: Evelyn Feng <evelyn2000feng@gmail.com>

--- Previos/Previo4/persona.hpp
+++ Previos/Previo4/persona.hpp
@@ -1,3 +1,5 @@
+//Previo 4 B82870 Evelyn F
+//header file; prototipo
 #ifndef PERSONA_H 
 #define PERSONA_H 
 
@@ -5,16 +7,16 @@
 
 using namespace std; 
 
-class Persona { 
+class Persona { //clase con atributos
 private: 
     string nombre; 
     int edad; 
 
 public: 
-    Persona(string nombre, int edad);
+    Persona(string nombre, int edad); //constructor
     string getNombre(); 
     int getEdad(); 
-    void setEdad(int edad); 
+    void setEdad(int edad); //recibe argumento de edad
 }; 
 
-#endif // PERSONA_H 
+#endif 
