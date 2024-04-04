1296250650680cbf3edebf657bc33ad55dc5cba3
Evelyn Feng <evelyn2000feng@gmail.com>
2024-04-04 08:17:39 -0600

    inclusion de archivos 1
    Signed-off-by: Evelyn Feng <evelyn2000feng@gmail.com>

---
+++ Laboratorios/Laboratorio2/Empleado.hpp
@@ -0,0 +1,27 @@
+//Laboratorio 2 Evelyn Feng Wu B82870
+
+#ifndef EMPLEADO_HPP
+#define EMPLEADO_HPP
+
+#include <string>
+
+class Empleado {
+    protected:
+        std::string nombre;
+        int edad;
+        double salario;
+
+    public: //metodos publicos
+        Empleado(std::string _string, int _edad, double _salario);
+
+        //crea virtuales
+        virtual ~Empleado(){} //destructor que limpia lo abierto al ejecutar el codigo
+        virtual double calcularPago() const=0;//virtual puro o tipico : si o si se debe sobreescribir
+        virtual void mostrarDetalles() const;// extra para la derivada
+};
+
+//virtual es lo q no esta definido en Empleado.cpp, se tiene q crear si o si se hace aca
+//en clase derivada puede rear su propio metodo = virtual, le cae encima a la clase
+//puro debe crear clase virtual de donde lo hereda
+//clase derivada define metodo virtuals
+#endif //EMPLEADO_HPP
