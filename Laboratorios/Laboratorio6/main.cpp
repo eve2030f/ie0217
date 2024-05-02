d3ab7cfa4e20ff86e2e6c89969bc0e209f9cd86e
Evelyn Feng <evelyn2000feng@gmail.com>
2024-05-02 07:34:29 -0600

    subiendo lab6 de templates
    Signed-off-by: Evelyn Feng <evelyn2000feng@gmail.com>

---
+++ Laboratorios/Laboratorio6/main.cpp
@@ -0,0 +1,74 @@
+//Laboratorio 6 Evelyn Feng Wu B82870 
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <stdexcept>
+
+template <typename T> //uso de template
+//clase estadistica
+class Statistics { 
+    public:
+    //constructor: const a elementos que no vamos a modificar. Recibe el data.
+        Statistics(const std::vector<T>&data) : data(data){ 
+            //vector vacio sale argumento invalido como excepcion
+            if(data.empty()){
+                throw std::invalid_argument("La lista de datos debe contener elementos"); //uso de excepcion
+            }
+        } 
+        //metodo de promedio de elementos
+        double mean() const{
+            T sum = 0;
+            for (const T& value : data){
+                sum += value;
+            }
+        return static_cast<double>(sum)/ data.size();// viene todo parametrizado
+        }
+        //metodo de desviacion estandar
+        double standardDeviation() const{
+            double meanValue = mean();// se calcula en el metodo anterior y aca lo llama
+            double sumSquares = 0;
+
+            for (const T& value : data){
+                sumSquares += (value -meanValue) * (value/meanValue);
+            }
+            return std::sqrt(sumSquares/data.size()); //retorna la formula de la desvest
+        }
+    private: //elemento privado dentro de la claseo clases amigas, variable T priv
+        std::vector<T> data;
+
+};
+
+int main(){
+    //se debe identficar con un catch diferente cada error
+    try{
+    //carga intData
+        std::vector<int> intData={1,2,3,4,5};
+        //instancia con template
+        
+        Statistics <int> statsInt(intData); //carga dato de data creado en el template, ejecuta desde el constructor
+
+        std::cout << "Media de intData: " <<statsInt.mean() <<std::endl;
+        std::cout << "Desviacion estandar de intData: " <<statsInt.standardDeviation() <<std::endl; 
+         //al ser un metodo se deben poner dos parentesis lueugo de llamarlo
+        //si no lo pone se vuelve un callable, ejecute la porcion de codigo q empiece en esta direccion de memoria
+
+
+    //carga doubleData    
+        std::vector<double>doubleData = {1.5, 2.5, 3.5 ,4.5, 5.5};
+        Statistics <double> statsDouble(doubleData); //cuidado con el <double> clase de var no cambia
+
+        std::cout << "Media de doubleData: " <<statsInt.mean() <<std::endl;
+        std::cout << "Desviacion estandar de doubleData: " <<statsInt.standardDeviation() <<std::endl;
+       
+
+    //crea vector de enteros
+        std::vector<int> emptyData;
+        Statistics<int> statsEmpty  (emptyData); 
+    } catch(const std::invalid_argument& e) {
+        std::cerr << "Excepcion encontrada: "<<e.what()<<std::endl; //el what funciona solo si en catch llama el invalid argument
+    }
+
+    return 0;
+}
+
+
